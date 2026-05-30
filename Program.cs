f659f5d6c2628797bda6e4c6d475a60c60acec74
Qhari Waman <qhariquinteros@gmail.com>
2026-05-29 21:05:48 -0300

    Resuelto ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,44 +1,36 @@
 ﻿Console.WriteLine("Hello, World!");
-
 int a;
-
 int b;
-
 a = 10;
-
 b = a;
-
 Console.WriteLine("Valor de a: " + a);
 Console.WriteLine("Valor de b: " + b);
-
-int numero, invertido = 0, temp, digito;
-bool resultado;
-string s;
+int numero, invertido = 0;
+bool valido;
 do
 {
     Console.WriteLine("Ingrese un numero");
-    s = Console.ReadLine();
-    resultado = int.TryParse(s, out numero);
-    if (resultado == true)
+    valido = int.TryParse(Console.ReadLine(), out numero);
+    if (!valido) // if (valido != true) ó if (valido == false) 
     {
-        if (numero > 0)
+        Console.WriteLine("El valor ingresado no es un numero.");
+    }
+    else
+    {
+        if (numero < 0)
         {
-            temp = numero;
+            Console.WriteLine("El numero debe ser mayor a 0.");
+        }
+        else
+        {
+            int temp = numero;
             while (temp > 0)
             {
-                digito = temp % 10;
+                int digito = temp % 10;
                 invertido = invertido * 10 + digito;
                 temp /= 10;
             }
             Console.WriteLine("Numero invertido: " + invertido);
         }
-        else
-        {
-            Console.WriteLine("El numero debe ser mayor a 0.");
-        }
     }
-    else
-    {
-        Console.WriteLine("El valor ingresado no es un numero.");
-    }
-} while (resultado != true || numero <= 0);
+} while (!valido || numero <= 0);
