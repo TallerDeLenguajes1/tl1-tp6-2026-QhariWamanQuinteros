d569a9a6ac1092ca5ec9c0d689c5321137c850fb
Qhari Waman <qhariquinteros@gmail.com>
2026-05-29 22:23:16 -0300

    Ejercicio 2 resuelto

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ a = 10;
 b = a;
 Console.WriteLine("Valor de a: " + a);
 Console.WriteLine("Valor de b: " + b);
-int numero, invertido = 0;
+int numero, temp, digito, invertido = 0;
 bool valido;
 do
 {
@@ -23,10 +23,10 @@ do
         }
         else
         {
-            int temp = numero;
+            temp = numero;
             while (temp > 0)
             {
-                int digito = temp % 10;
+                digito = temp % 10;
                 invertido = invertido * 10 + digito;
                 temp /= 10;
             }
@@ -34,3 +34,78 @@ do
         }
     }
 } while (!valido || numero <= 0);
+int opcion, num1, num2, resultado = 0;
+do
+{
+    do
+    {
+        Console.WriteLine("Ingrese la operacion que desea realizar");
+        Console.WriteLine("\t1. Sumar");
+        Console.WriteLine("\t2. Restar");
+        Console.WriteLine("\t3. Multiplicar");
+        Console.WriteLine("\t4. Dividir");
+        valido = int.TryParse(Console.ReadLine(), out opcion);
+        if (!valido)
+        {
+            Console.WriteLine("El valor ingresado no es un numero.");
+        }
+        else
+        {
+            if (opcion < 1 || opcion > 4)
+            {
+                Console.WriteLine("El numero ingresado no es una opcion valida");
+            }
+        }
+    } while (!valido || opcion < 1 || opcion > 4);
+    do
+    {
+        Console.WriteLine("Ingrese un numero");
+        valido = int.TryParse(Console.ReadLine(), out num1);
+        if (!valido)
+        {
+            Console.WriteLine("El valor ingresado no es un numero.");
+        }
+    } while (!valido);
+    do
+    {
+        Console.WriteLine("Ingrese otro numero");
+        valido = int.TryParse(Console.ReadLine(), out num2);
+        if (!valido)
+        {
+            Console.WriteLine("El valor ingresado no es un numero.");
+        }
+        else
+        {
+            if (num2 == 0 && opcion == 4)
+            {
+                Console.WriteLine("La division entre 0 (cero) no esta definida");
+            }
+        }
+    } while (!valido || (num2 == 0 && opcion == 4));
+    switch (opcion)
+    {
+        case 1: resultado = num1 + num2; break;
+        case 2: resultado = num1 - num2; break;
+        case 3: resultado = num1 * num2; break;
+        case 4: resultado = num1 / num2; break;
+    }
+    Console.WriteLine("El resultado es " + resultado);
+    do
+    {
+        Console.WriteLine("Desea realizar otra operacion?");
+        Console.WriteLine("\t1. Si");
+        Console.WriteLine("\t2. No");
+        valido = int.TryParse(Console.ReadLine(), out opcion);
+        if (!valido)
+        {
+            Console.WriteLine("El valor ingresado no es un numero.");
+        }
+        else
+        {
+            if (opcion < 1 || opcion > 2)
+            {
+                Console.WriteLine("El numero ingresado no es una opcion disponible");
+            }
+        }
+    } while (!valido || opcion < 1 || opcion > 2);
+} while (opcion != 2);
